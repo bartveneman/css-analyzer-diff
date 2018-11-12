44e66a26619a28e8e0156f2e769f862a177cfc08
Bart Veneman <bartveneman@users.noreply.github.com>
2018-11-12 22:46:22 +0100

    Keep order of font-sizes and colors intact after comparison

--- src/diff-lists.js
+++ src/diff-lists.js
@@ -1,21 +1,40 @@
-function mergeLists(listA, listB) {
-	// Create a single list out of both lists that will take their
-	// unique values
-	return (
-		listA
-			.concat(listB)
-			// Re-sort the items
-			.sort((a, b) => {
-				return a.toLowerCase().localeCompare(b.toLowerCase())
-			})
-			// Remove duplicate items
-			.filter((value, index, self) => {
-				return self.lastIndexOf(value) === index
-			})
-	)
+const cssUnitSort = require('css-unit-sort')
+const cssColorSort = require('color-sorter')
+
+const METRICS_SORTED_BY_CSS_UNIT = ['values.fontsizes.unique']
+const METRICS_SORTED_BY_COLOR = [
+	'values.colors.unique',
+	'values.colors.duplicates'
+]
+
+function getSortingFnByKey(key) {
+	if (METRICS_SORTED_BY_CSS_UNIT.includes(key)) {
+		return cssUnitSort.sortFn
+	}
+
+	return (a, b) => {
+		return b.toLowerCase().localeCompare(a.toLowerCase())
+	}
 }
 
-function diffLists(listA, listB) {
+function mergeLists(listA, listB, key) {
+	// Create a single list out of both lists that will take their
+	// unique values
+	const merged = listA
+		.concat(listB)
+		// Remove duplicate items
+		.filter((value, index, self) => {
+			return self.lastIndexOf(value) === index
+		})
+		// Re-sort the items
+		.sort(getSortingFnByKey(key))
+		.reverse()
+
+	// Until color-sorter supports a sortFn, we must sort the list at this place
+	return METRICS_SORTED_BY_COLOR.includes(key) ? cssColorSort(merged) : merged
+}
+
+function diffLists(listA, listB, key) {
 	if (typeof listB === 'undefined') {
 		listB = listA
 		listA = undefined
@@ -26,7 +45,7 @@ function diffLists(listA, listB) {
 
 	// The diff will be the combined list with an indication per item
 	// whether it was added, removed or changed
-	const diff = mergeLists(listA, listB).map(item => {
+	const diff = mergeLists(listA, listB, key).map(item => {
 		const itemExistsInA = listA.includes(item)
 		const itemExistsInB = listB.includes(item)
 
