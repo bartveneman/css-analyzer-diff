f71d302196fc5964dd2b482cdd37dd358da50e36
Bart Veneman <bartveneman@users.noreply.github.com>
2019-09-01 14:09:50 +0200

    🔥 BREAKING: update dependencies

--- src/diff-strings.js
+++ src/diff-strings.js
@@ -1,21 +1,21 @@
 function diffStrings(stringA, stringB) {
-	// Make sure the first string is undefined if one of them is undefined
-	if (typeof stringB === 'undefined') {
-		stringB = stringA
-		stringA = undefined
-	}
+  // Make sure the first string is undefined if one of them is undefined
+  if (typeof stringB === "undefined") {
+    stringB = stringA;
+    stringA = undefined;
+  }
 
-	if (typeof stringA === 'undefined') {
-		stringA = null
-	}
+  if (typeof stringA === "undefined") {
+    stringA = null;
+  }
 
-	const diff = {
-		oldValue: stringA,
-		newValue: stringB,
-		changed: stringA !== stringB
-	}
+  const diff = {
+    oldValue: stringA,
+    newValue: stringB,
+    changed: stringA !== stringB
+  };
 
-	return diff
+  return diff;
 }
 
-module.exports = diffStrings
+module.exports = diffStrings;
--- test/diff-strings.js
+++ test/diff-strings.js
@@ -1,35 +1,46 @@
-const test = require('ava')
-const diff = require('../src/diff-strings.js')
+const test = require("ava");
+const diff = require("../src/diff-strings.js");
 
-test('It structures the diff for string stats correctly', t => {
-	const result = diff('a', 'b')
+test("It structures the diff for string stats correctly", t => {
+  const result = diff("a", "b");
 
-	// Old and new values
-	t.true(typeof result.oldValue === 'string')
-	t.true(typeof result.newValue === 'string')
+  // Old and new values
+  t.true(typeof result.oldValue === "string");
+  t.true(typeof result.newValue === "string");
 
-	// Changed
-	t.is('boolean', typeof result.changed)
-})
+  // Changed
+  t.is("boolean", typeof result.changed);
+});
 
-test('It handles the first string stat being absent', t => {
-	const actual = diff(undefined, 'a')
-	const expected = {
-		oldValue: null,
-		newValue: 'a',
-		changed: true
-	}
+test("It handles the first string stat being absent", t => {
+  const actual = diff(undefined, "a");
+  const expected = {
+    oldValue: null,
+    newValue: "a",
+    changed: true
+  };
 
-	t.deepEqual(actual, expected)
-})
+  t.deepEqual(actual, expected);
+});
 
-test('It handles the second numeric stat being absent', t => {
-	const actual = diff('a', undefined)
-	const expected = {
-		oldValue: null,
-		newValue: 'a',
-		changed: true
-	}
+test("It handles the second numeric stat being absent", t => {
+  const actual = diff("a", undefined);
+  const expected = {
+    oldValue: null,
+    newValue: "a",
+    changed: true
+  };
 
-	t.deepEqual(actual, expected)
-})
+  t.deepEqual(actual, expected);
+});
+
+test("it handles correctly when there are no changes", t => {
+  const actual = diff("a", "a");
+  const expected = {
+    oldValue: "a",
+    newValue: "a",
+    changed: false
+  };
+
+  t.deepEqual(actual, expected);
+});
