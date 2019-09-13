d830fb8a6c00dd6a8b917f60557d09e24fbfb4ab
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2019-09-13 16:06:26 +0200

    replace prettier with eslint (#17)

--- src/diff-numbers.js
+++ src/diff-numbers.js
@@ -1,34 +1,34 @@
 function diffNumbers(numberA, numberB) {
-  // Make sure the first number is undefined if one of them is undefined
-  if (typeof numberB === "undefined") {
-    numberB = numberA;
-    numberA = undefined;
-  }
+	// Make sure the first number is undefined if one of them is undefined
+	if (typeof numberB === 'undefined') {
+		numberB = numberA
+		numberA = undefined
+	}
 
-  if (typeof numberA === "undefined") {
-    numberA = null;
-  }
+	if (typeof numberA === 'undefined') {
+		numberA = null
+	}
 
-  const diff = {
-    oldValue: numberA,
-    newValue: numberB,
-    changed: numberA !== numberB,
-    diff: {
-      absolute: numberB - numberA,
-      relative: (numberB - numberA) / numberA
-    }
-  };
+	const diff = {
+		oldValue: numberA,
+		newValue: numberB,
+		changed: numberA !== numberB,
+		diff: {
+			absolute: numberB - numberA,
+			relative: (numberB - numberA) / numberA
+		}
+	}
 
-  if (numberA === 0) {
-    diff.diff.relative = 1;
-  }
+	if (numberA === 0) {
+		diff.diff.relative = 1
+	}
 
-  if (numberA === null) {
-    diff.diff.absolute = numberB;
-    diff.diff.relative = 1;
-  }
+	if (numberA === null) {
+		diff.diff.absolute = numberB
+		diff.diff.relative = 1
+	}
 
-  return diff;
+	return diff
 }
 
-module.exports = diffNumbers;
+module.exports = diffNumbers
